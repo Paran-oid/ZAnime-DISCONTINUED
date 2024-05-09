88addff48710d8d48f250e579071b5df51d25f0e
Paran-oid <AzizHmid10000@gmail.com>
2024-05-09 20:48:05 +0100

    Add project files.

---
+++ Shared/Models/Main/Anime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models.Main
+{
+    public class Anime
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateOnly ReleaseDate { get; set; }
+        public DateOnly? EndDate { get; set; } = null;
+        public string Genre { get; set; }
+        public string MainPicturePath { get; set; }
+        public string BackgroundPath { get; set; }
+        public string PicturesPath { get; set; }
+        public string Description { get; set; }
+        public float Rating { get; set; }
+        public List<Character> Characters { get; set; }
+        public List<Actor> Actors { get; set; }
+        public List<Comment>? Comments { get; set; }
+
+    }
+}
