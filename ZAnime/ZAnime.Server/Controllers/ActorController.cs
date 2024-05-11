7612f3aa55a33b0862584dff7551d4c2bdf0ff05
Paran-oid <AzizHmid10000@gmail.com>
2024-05-11 09:38:57 +0100

    New Project Template

---
+++ ZAnime/ZAnime.Server/Controllers/ActorController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ZAnime.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AcotController : ControllerBase
+    {
+    }
+}
---
+++ ZAnime/ZAnime.Server/Controllers/CommentController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Data;
+
+namespace ZAnime.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+    }
+}
